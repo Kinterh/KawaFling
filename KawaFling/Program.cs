1b21173850400d6e603ccb75f71d7c46aa5b1869
Kinterh <in010210@gmail.com>
2018-05-14 19:29:44 +0900

    Consumer_secret 파일 숨김

--- KawaFling/Program.cs
+++ KawaFling/Program.cs
@@ -2,14 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
-
+using System.Threading;
+using System.Net.Http;
+using System.Net.Sockets;
+using System.Net;
+using System.IO;
 namespace KawaFling
 {
     class Program
     {
-        static void Main(string[] args)
+
+        public static void Main(string[] args)
         {
+            using (StreamReader read = new StreamReader("../../../config.txt", Encoding.UTF8))
+            {
+                TwitterRequest.Consumer_secret = read.ReadLine();
+            }
+          
         }
+        
     }
 }
