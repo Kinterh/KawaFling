aa01e7b5b5fa17230cb0acedea3b272abd6212cf
Kinterh <in010210@gmail.com>
2018-05-14 20:02:02 +0900

    Timestamp, Nonce 받아오는걸 함수로 분리

--- KawaFling/TwitterRequest.cs
+++ KawaFling/TwitterRequest.cs
@@ -3,12 +3,18 @@ using System.Text;
 using System.Net;
 using System.Security.Cryptography;
 using System.Globalization;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
 
 namespace KawaFling
 {
     class TwitterRequest
     {
-        
+        static string oauth_token;
+        static string oauth_token_secret;
+        static string oauth_verifier;
+
         static string Callback = "oob";
         static string ConsumerKey = "xUV8UwzpJk4aMyTFzJXXXv4eQ";
         static string SignatureMethod = "HMAC-SHA1";
@@ -16,10 +22,15 @@ namespace KawaFling
 
         public static string Consumer_secret {private get; set; }
 
-        public static WebResponse GetRequestToken()
+        public static void GetAccessToken()
         {
-            string Timestamp = ((int)(DateTime.UtcNow - new DateTime(1970, 1, 1)).TotalSeconds).ToString();
-            string Nonce = Convert.ToBase64String(Encoding.UTF8.GetBytes(Timestamp + Timestamp + Timestamp));
+
+        }
+
+        public static void GetRequestToken()
+        {
+            string Timestamp = GetTimestamp();
+            string Nonce = GetNonce(Timestamp);
             string SignatureBaseString = GetSignatureBaseString(Timestamp, Nonce);
             string SHA1 = GetSha1Hash(Consumer_secret, SignatureBaseString);
 
@@ -40,8 +51,29 @@ namespace KawaFling
 
             httpWebRequest.Headers.Add("Authorization", Header);
 
-            var Result = httpWebRequest.GetResponse();
-            return Result;
+            var webResponse = httpWebRequest.GetResponse();
+
+            Stream responseStream = webResponse.GetResponseStream();
+            string responseBody = String.Empty;
+            if (responseStream != null) responseBody = new StreamReader(responseStream).ReadToEnd();
+
+            oauth_token = ParseQuerystringParameter("oauth_token", responseBody);
+            oauth_token_secret = ParseQuerystringParameter("oauth_token_secret", responseBody);
+            oauth_verifier = ParseQuerystringParameter("oauth_verifier", responseBody);
+        }
+
+        static private string GetTimestamp() { return ((int)(DateTime.UtcNow - new DateTime(1970, 1, 1)).TotalSeconds).ToString(); }
+        static private string GetNonce(string Timestamp) { return Convert.ToBase64String(Encoding.UTF8.GetBytes(Timestamp + Timestamp + Timestamp)); }
+        private static string ParseQuerystringParameter(string parameterName, string text)
+        {
+            Match expressionMatch = Regex.Match(text, string.Format(@"{0}=(?<value>[^&]+)", parameterName));
+
+            if (!expressionMatch.Success)
+            {
+                return string.Empty;
+            }
+
+            return expressionMatch.Groups["value"].Value;
         }
 
         public static string GetSha1Hash(string key, string message)
